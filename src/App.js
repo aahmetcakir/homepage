43a93bfaa312b3b84041ffed6b30a081655fc98f
ahmetcakir <ca.ahmetcakir@gmail.com>
2022-12-14 00:55:50 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
         <h1>Ahmet ÇAKIR</h1>
         <div className='links'>
           <h6>
-            GitHub : <a href="https://github.com/aahmetcakir" target="_blank" rel='noreferrer' className='mail'>aahmetcakir</a>
+            GitHub: <a href="https://github.com/aahmetcakir" target="_blank" rel='noreferrer' className='mail'>aahmetcakir</a>
           </h6>
           <h6>
-            contact me : <a href="mailto:ca.ahmetcakir@gmail.com" target="_blank" rel='noreferrer' className='mail'>ca.ahmetcakir@gmail.com</a>
+            Contact me: <a href="mailto:ca.ahmetcakir@gmail.com" target="_blank" rel='noreferrer' className='mail'>ca.ahmetcakir@gmail.com</a>
           </h6>
           <h6>
-            Linkedin : <a href="https://www.linkedin.com/in/aahmetcakir/" target="_blank" rel='noreferrer' className='mail'>aahmetcakir</a>
+            Linkedin: <a href="https://www.linkedin.com/in/aahmetcakir/" target="_blank" rel='noreferrer' className='mail'>aahmetcakir</a>
           </h6>
         </div>
 
